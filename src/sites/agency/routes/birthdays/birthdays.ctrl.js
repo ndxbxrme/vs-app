e09a801fe10e2adf94b5977d567b4aa63e2fc13c
ndxbxrme <lewis_the_cat@hotmail.com>
2023-09-08 16:52:21 +0100

    sort by date and image fix

--- src/sites/agency/routes/birthdays/birthdays.ctrl.js
+++ src/sites/agency/routes/birthdays/birthdays.ctrl.js
@@ -56,6 +56,6 @@ angular.module('vs-agency').controller('agencyBirthdaysCtrl', function($scope) {
     const then = new Date(birthday);
     const now = new Date();
     const age = Math.floor((now - then) / 1000 / 60 / 60/ 24 / 365);
-    return age + ' Year' + (age > 1 ? 's':'');
+    return age + ' Year' + (age === 1 ? '':'s');
   }
 })
